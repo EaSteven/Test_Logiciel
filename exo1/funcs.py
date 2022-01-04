30022558105dea2a3eb32469faa57a8fbfdf2b96
Steven <easteven94700@gmail.com>
2022-01-05 00:10:55 +0100

    add get_mean tests and prototype

---
+++ exo1/funcs.py
@@ -0,0 +1,2 @@
+def get_mean(tab_int=[]):
+	return -1
---
+++ exo1/tests_funcs.py
@@ -0,0 +1,15 @@
+import funcs
+import unittest
+
+class TestFuncs(unittest.TestCase):
+
+	def test_max_int(self):
+		self.assertEqual(funcs.get_mean([10,12,1,1]),6)
+		self.assertEqual(funcs.get_mean([0,1,2,3,4]),2)
+		self.assertEqual(funcs.get_mean([-1,-1,-1,-1]),-1)
+		self.assertEqual(funcs.get_mean([-1,-2,1,2]),0)
+		self.assertEqual(funcs.get_mean([2.5,2.5,2.5,2.5]),2)
+		
+
+if __name__ == '__main__':
+	unittest.main()
