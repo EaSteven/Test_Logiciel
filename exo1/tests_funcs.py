3049e72415e4e3c0e9990fe8c64e086e83833259
Steven <easteven94700@gmail.com>
2022-01-05 00:31:08 +0100

    corrected name of test_get_mean

--- exo1/tests_funcs.py
+++ exo1/tests_funcs.py
@@ -3,13 +3,12 @@ import unittest
 
 class TestFuncs(unittest.TestCase):
 
-	def test_max_int(self):
+	def test_get_mean(self):
 		self.assertEqual(funcs.get_mean([10,12,1,1]),6)
 		self.assertEqual(funcs.get_mean([0,1,2,3,4]),2)
 		self.assertEqual(funcs.get_mean([-1,-1,-1,-1]),-1)
 		self.assertEqual(funcs.get_mean([-1,-2,1,2]),0)
 		self.assertEqual(funcs.get_mean([2.5,2.5,2.5,2.5]),2)
 		
-
 if __name__ == '__main__':
 	unittest.main()
