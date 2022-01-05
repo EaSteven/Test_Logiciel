2168f70e35895769f7cb2a0b5d2710ad15799a78
Steven <easteven94700@gmail.com>
2022-01-05 16:11:39 +0100

    add suite_Arithm/Geo function

--- exo1/funcs.py
+++ exo1/funcs.py
@@ -1,4 +1,5 @@
 import statistics
+
 def get_mean(tab_int=[]):
 	tab_sum=0
 	for value in tab_int:
@@ -32,7 +33,47 @@ def is_Géo(tab_int):
 	return True
 
 def suite_Arithm(tab_int,n):
-	return -1
+	int_output=[]
+	raison = tab_int[1]-tab_int[0]
+	for i in range (len(tab_int)-1):
+		if (tab_int[i+1]-tab_int[i] != raison):
+			bool_output = False
+			break
+		else:
+			bool_output = True		
+
+	if (bool_output == True):
+		if (n>0):
+			for i in range (n):
+				int_output.append(tab_int[len(tab_int)-1] + raison*(i+1))   
+		else:
+			int_output.append(None)
+	else:
+		int_output.append(None)
+	return bool_output,int_output
 
 def suite_Géo(tab_int,n):
+	int_output=[]
+	raison = tab_int[1]/tab_int[0]
+	for i in range (len(tab_int)-1):
+		if (tab_int[i+1]/tab_int[i] != raison):
+			bool_output = False
+			break
+		else:
+			bool_output = True		
+
+	if (bool_output == True):
+		if (n>0):
+			for i in range (n):
+				if (raison == 1):
+					int_output.append(tab_int[1])
+				elif (raison == 0):
+					int_output.append(tab_int[0])  
+				else:
+					int_output.append(tab_int[len(tab_int)-1] * raison*(i+1))   
+		else:
+			int_output.append(None)
+	else:
+		int_output.append(None)
+	return bool_output,int_output
 	return -1
