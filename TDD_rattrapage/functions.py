5de7d8c255d92cf0978d5efc79cf45aba0599a75
Steven <easteven94700@gmail.com>
2022-01-28 16:52:25 +0100

    add function mirror

--- TDD_rattrapage/functions.py
+++ TDD_rattrapage/functions.py
@@ -1,3 +1,21 @@
 
 def mirror(str_input,nb):
-	return -1
+	# check if nb is a integer
+	if isinstance(nb,float):
+		return -1
+	# check if integer is not > to the str_input. 
+	if (nb > len(str_input)):
+		return -1
+	#check if integer is not negatif
+	if (nb < 0):
+		return -1
+
+	return_string = str_input[:nb+1]
+	inverse_string = return_string
+	
+	while nb != 0:
+		return_string += inverse_string[nb]
+		nb -= 1
+	#last iter for nb = 0
+	return_string += inverse_string[nb]
+	return return_string
